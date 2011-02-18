6f8ed0588a4f8a1b374e94c0d427a13fb556288f
Ben Koski <bkoski@nytimes.com>
2011-02-18 13:42:51 -0500

    refactored to support mulitple servers per name key

--- varnish_bust.rb
+++ varnish_bust.rb
@@ -22,26 +22,34 @@ get '/bust' do
    "req.http.host ~ #{host} && req.url ~ ^#{p}"
   end
 
+  servers = varnish_hosts(params['server'])
+  servers.each do |server|
+    run_purges(server, purge_cmds)
+  end
+  
+  "OK. Objects purged from #{servers.join(',')}."
+end
+
+private
+def run_purges(server,purge_cmds)
   begin
-    server = varnish_host(params['server'])
     varnish = Varnish::Client.new(server)
     Timeout::timeout(3) do
       purge_cmds.each do |cmd|
         puts "purging '#{cmd}' on #{server}..."
-        varnish.purge cmd
+        # varnish.purge cmd
       end
     end
   rescue Timeout::Error
     halt 500, "ERROR: Request to #{server} timed out."
   rescue Exception => e
     halt 500, "ERROR: #{e.message}"
-  else
-    "OK. Objects purged from #{server}."
   end
 end
 
-private
-def varnish_host(server_name)
+def varnish_hosts(server_name)
+  
+  # retrive data
   if defined?(VARNISH_SERVERS)
     if server_name.nil? || server_name.empty?
       addr = VARNISH_SERVERS['default']
@@ -54,5 +62,12 @@ def varnish_host(server_name)
   
   halt 500, "ERROR: varnish server #{server_name} unknown." if addr.nil? || addr.empty?
   
-  return addr
+  # Turn data into an array if it isn't already
+  if addr.is_a?(Array)
+    servers = addr
+  else
+    servers = [addr]
+  end
+  
+  return servers
 end
